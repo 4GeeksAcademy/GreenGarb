d6938bd77cc6da7d1072eb54cc4fe9e8f56f4a66
kommercialkings <122939782+kommercialkings@users.noreply.github.com>
2023-08-10 23:30:55 +0000

    contactpage

--- src/front/js/pages/contactpage.js
+++ src/front/js/pages/contactpage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../styles/contacpage.css";
 
 
 export function ContactPage(){
@@ -7,18 +8,19 @@ export function ContactPage(){
 
  
     return(
-        <div>
-            <h1></h1>
+        <div className="contact">
+        
             
-            <h2>Get in Touch</h2>
+        
             <form action="submit_form.php" method="post">
+            <h2>Get in Touch</h2>
                 <label for="name">Name:</label>
                 <input type="text" id="name" name="name" required/>
                 <label for="email">Email:</label>
                 <input type="email" id="email" name="email" required/>
                 <label for="message">Message:</label>
                 <textarea id="message" name="message" rows="4" required></textarea>
-                <button type="submit">Send</button>
+                <button type="submit" className="send">Send</button>
                 
             </form>
        

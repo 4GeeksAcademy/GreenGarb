444529348892687d3c3128296c42f5250fd0497e
Yvv1214 <39809411+Yvv1214@users.noreply.github.com>
2023-08-13 22:59:32 +0000

    aligned nav input

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -7,21 +7,25 @@ import "../../styles/navbar.css"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-success p-0">
-			<div className="container-fluid justify-content-between">
+		<nav className="navbar navbar-light nav-color p-0">
+			<div className="container-fluid justify-content-between align-middle">
 				<div className="navbar-brand">
-					<Link to="/Contact">
-						<div className="nav-logo"></div>
+					<Link to="/">
+						<div className="nav-logo">greengarb</div>
 					</Link>
 				</div>
 
-				<div className="d">
-					<input type="text" placeholder="search"></input>
+				<div className="align-middle input-div col-5">
+					<input classname="align-middle w-100" type="text" placeholder="search"></input>
 				</div>
 
 				<div className="icons position-absolute end-0 me-5 justify-content-between">
-					<i class="fas fa-heart fa-xl me-2"></i>
-					<i class="fas fa-cart-arrow-down fa-xl me-2"></i>
+					<i class="fas fa-heart fa-xl me-2">
+						<Link to='/favorites'></Link>
+					</i>
+					<i class="fas fa-cart-arrow-down fa-xl me-2">
+						<Link to='/cart'></Link>
+					</i>
 					<i class="fas fa-user fa-xl">
 						<Link to="/user"></Link>
 					</i>

7e39c9eeef31873a2b5a8f643f0da58b370ad9c6
kommercialkings <122939782+kommercialkings@users.noreply.github.com>
2023-08-16 22:13:40 +0000

    new footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import "../../styles/footer.css"
 
 export const Footer = () => {
 	const year = new Date().getFullYear();
 
-	return (<footer className="footer text-center">{`Copyright © Upbeat Code ${year}`}</footer>)
+	return (<footer className="footer mt-auto py-3 text-center">
+		<p>Shop Sell About Help</p>
+
+
+
+	</footer>)
 };
+
 
 
 

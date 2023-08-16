bdcd00797bf863f7e323705bef64b9f0ecf20a7e
kommercialkings <122939782+kommercialkings@users.noreply.github.com>
2023-08-16 01:19:52 +0000

    update styling to contact us

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,15 +1,4 @@
 import React, { Component } from "react";
-
-<<<<<<< HEAD
-export const Footer = () => (
-	<footer className="footer mt-5 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			Ricardo and The Crew 
-		</p>
-	</footer>
-);
-=======
 export const Footer = () => {
 	const year = new Date().getFullYear();
 
@@ -18,6 +7,3 @@ export const Footer = () => {
 
 
 
-
-
->>>>>>> refs/remotes/origin/main

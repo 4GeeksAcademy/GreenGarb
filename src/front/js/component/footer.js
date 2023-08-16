942b8363fc511f7741232e8b00f105e72941c0e9
kommercialkings <122939782+kommercialkings@users.noreply.github.com>
2023-08-16 01:05:45 +0000

    update styling for contact page

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-5 text-center">
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
 			Ricardo and The Crew 

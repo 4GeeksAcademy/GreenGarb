c2e1de4bc5e6e2f5fc133020f23aa593f85ca494
kommercialkings <122939782+kommercialkings@users.noreply.github.com>
2023-08-18 00:37:29 +0000

    newnewfooter

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -5,12 +5,69 @@ export const Footer = () => {
 	const year = new Date().getFullYear();
 
 	return (<footer className="footer mt-auto py-3 text-center">
-		<p>Shop Sell About Help</p>
+		<div class="shop">
+			<div class="card-header">
+				Shop
+			</div>
+			<div class="card-body">
 
+				<p>site map</p>
+				<p>register</p>
+
+
+			</div>
+		</div>
+		<div class="shop">
+			<div class="card-header">
+				Sell
+			</div>
+			<div class="card-body">
+				<p>
+					seller faq
+				</p>
+
+				<p>start selling</p>
+
+			</div>
+		</div>
+
+
+		<div class="shop">
+			<div class="card-header">
+				about
+			</div>
+			<div class="card-body">
+				<p>
+					about us
+				</p>
+
+				<p>our ethical approach</p>
+
+			</div>
+		</div>
+		<div class="shop">
+			<div class="card-header">
+				Help
+			</div>
+			<div class="card-body">
+				<p>
+					contact us
+				</p>
+
+			</div>
+		</div>
+		{/* <div>
+			<p>All Rights Reserved</p>
+
+		</div> */}
 
 
 	</footer>)
 };
+
+
+
+
 
 
 

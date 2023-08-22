3d0dc572110758a7e2f2890bca3929c9ee088234
kommercialkings <122939782+kommercialkings@users.noreply.github.com>
2023-08-22 18:20:52 +0000

    updated footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -5,38 +5,43 @@ export const Footer = () => {
 	const year = new Date().getFullYear();
 
 	return (
-		<div class="footer content">
+		<div className="footer-content footer content">
 
 			<footer>
-				<div class="container">
-					<div class="row justify-content-center">
-						<div class="col-sm-4 col-md-3 item">
+				<div className="container">
+					<div className="row justify-content-center">
+						<div className="col-sm-4 col-md-3 item">
 							<h3>Shop</h3>
 							<ul>
 								<li><a href="#">Register</a></li>
 							</ul>
 						</div>
-						<div class="col-sm-4 col-md-3 item">
+						<div className="col-sm-4 col-md-3 item">
 							<h3>Sell</h3>
 							<ul>
 								<li><a href="#">Start Selling</a></li>
 							</ul>
 						</div>
-						<div class="col-sm-4 col-md-3 item">
+						<div className="col-sm-4 col-md-3 item">
 							<h3>About</h3>
 							<ul>
 								<li><a href="#">Our Ethical Approach</a>                              </li>
 							</ul>
 						</div>
-						<div class="col-sm-4 col-md-3 item">
+						<div className="col-sm-4 col-md-3 item">
 							<h3>Help</h3>
 							<ul>
-								<li><a href="#">Contact Us</a></li>
+								<li><a href="#"> Contact Us</a></li>
 							</ul>
+							
 						</div>
+						<p className="copyright">copyright 2023</p> 
 					</div>
+
 				</div>
+
 			</footer>
+
 		</div>
 	)
 };

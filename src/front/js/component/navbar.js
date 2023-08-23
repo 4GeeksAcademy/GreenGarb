88e4037f79f7c9aaa35f100b8d22b450f7c1118f
Yvv1214 <39809411+Yvv1214@users.noreply.github.com>
2023-08-23 04:10:15 +0000

    linked nav icons

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -20,19 +20,13 @@ export const Navbar = () => {
 				</div>
 
 				<div className="align-middle input-div col-5">
-					<input classname="nav-input w-100  m-0" type="text" placeholder="search"></input>
+					<input classname="nav-input w-100" type="text" placeholder="search"></input>
 				</div>
 
 				<div className="icons position-absolute end-0 me-5 justify-content-between">
-					<i class="fas fa-heart fa-xl me-2">
-						<Link to='/favorites'></Link>
-					</i>
-					<i class="fas fa-cart-arrow-down fa-xl me-2">
-						<Link to='/cart'></Link>
-					</i>
-					<i class="fas fa-user fa-xl">
-						<Link to="/user"></Link>
-					</i>
+					<Link to='/favorites'><i className="fas fa-heart fa-xl me-2"></i></Link>
+					<Link to='/cart'><i className="fas fa-cart-arrow-down fa-xl me-2"></i></Link>
+					<Link to="/user"><i className="fas fa-user fa-xl"></i></Link>
 				</div>
 
 				<div className="ml-auto">
@@ -46,37 +40,37 @@ export const Navbar = () => {
 
 		<nav className="navbar navbar-light bg-light lower-nav justify-content-start mb-5">
 
-			<div class="dropdown ms-5 me-3">
-			<button class="btn btn-light border border-dark dropdown-toggle" type="button" id="MensDropDown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			<div className="dropdown ms-5 me-3">
+			<button className="btn btn-light border border-dark dropdown-toggle" type="button" id="MensDropDown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				Mens
 			</button>
-			<div class="dropdown-menu" aria-labelledby="MensDropDown">
-				<a class="dropdown-item" href="#">Tops</a>
-				<a class="dropdown-item" href="#">Bottoms</a>
+			<div className="dropdown-menu" aria-labelledby="MensDropDown">
+				<a className="dropdown-item" href="#">Tops</a>
+				<a className="dropdown-item" href="#">Bottoms</a>
 			</div>
 			</div>
 
 
 			
-			<div class="dropdown me-3">
-			<button class="btn btn-light border border-dark dropdown-toggle" type="button" id="WomensDropDown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			<div className="dropdown me-3">
+			<button className="btn btn-light border border-dark dropdown-toggle" type="button" id="WomensDropDown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				Womens
 			</button>
-			<div class="dropdown-menu" aria-labelledby="WomensDropDown">
-				<a class="dropdown-item" href="#">Tops</a>
-				<a class="dropdown-item" href="#">Bottoms</a>
+			<div className="dropdown-menu" aria-labelledby="WomensDropDown">
+				<a className="dropdown-item" href="#">Tops</a>
+				<a className="dropdown-item" href="#">Bottoms</a>
 			</div>
 			</div>
 
 
-			<div class="dropdown me-3">
-			<button class="btn btn-light border border-dark dropdown-toggle" type="button" id="ShoesDropDown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			<div className="dropdown me-3">
+			<button className="btn btn-light border border-dark dropdown-toggle" type="button" id="ShoesDropDown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 				Shoes
 			</button>
-			<div class="dropdown-menu" aria-labelledby="ShoesDropDown">
-				<a class="dropdown-item" href="#">Sneakers</a>
-				<a class="dropdown-item" href="#">Boots</a>
-				<a class="dropdown-item" href="#">Sandals</a>
+			<div className="dropdown-menu" aria-labelledby="ShoesDropDown">
+				<a className="dropdown-item" href="#">Sneakers</a>
+				<a className="dropdown-item" href="#">Boots</a>
+				<a className="dropdown-item" href="#">Sandals</a>
 			</div>
 			</div>
 		</nav>

a0003ef0faf60db78cc0e9c1e70e3655510710ff
Yvv1214 <39809411+Yvv1214@users.noreply.github.com>
2023-08-26 04:19:34 +0000

    login page responsive

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -38,7 +38,7 @@ export const Login = () => {
 			<h1>Please Sign In </h1>
 			
 
-			<form className="container w-25 p-4 bg-light mb-5" onSubmit={(e) => submit(e)}>
+			<form className="container col-md-4 p-4 bg-light mb-5 login-form" onSubmit={(e) => submit(e)}>
 			<div className="form-group ">
 				<label for="userName">Username</label>
 				<input type="text" className="form-control" id="userName" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)}></input>

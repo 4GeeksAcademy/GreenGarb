2a8defdfc04ccb4582873cf301ab8e5ec85c88b5
Yvv1214 <39809411+Yvv1214@users.noreply.github.com>
2023-08-28 03:30:46 +0000

    adding blank profile

--- src/front/js/pages/yourshop.js
+++ src/front/js/pages/yourshop.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import blankProfile from "../../img/blankProfile"
 import { User } from "./user";
 
 
@@ -33,7 +34,7 @@ export const Yourshop = () => {
 
         <>
             <div className="user-header-div justify-content-center d-flex mb-3">
-                <div className="user-img-div rounded-circle">
+                <div className="user-img-div rounded-circle" style={{ backgroundImage:`url(${blankProfile})` }}>
                     {/* <img className="user-img" src="https://images.pexels.com/photos/4355345/pexels-photo-4355345.jpeg?auto=compress&cs=tinysrgb&w=600"/> */}
                 </div>
                     <h2 className="ms-2 align-self-center"><i class="fa-solid fa-store"></i>shop name</h2>

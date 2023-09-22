b09f19655ae583153909123b02501d6fee66f161
Yvv1214 <39809411+Yvv1214@users.noreply.github.com>
2023-09-22 20:09:35 +0000

    fixed contact page inputs

--- src/front/js/pages/contactpage.js
+++ src/front/js/pages/contactpage.js
@@ -14,15 +14,17 @@ export function ContactPage(){
         
             <form className= "page justify-content-center" action="submit_form.php" method="post">
             <h2 className="container mt-4 ps-5" > Get in Touch </h2>
+            <div style={{width: 'fit-content'}}>
                 <label for="name">Name:</label>
-                <input type="text" id="name" placeholder="Your Name" name="name" required/>
+                <input className="w-100" type="text" id="name" placeholder="Your Name" name="name" required/>
                 <label for="email">Email:</label>
-                <input type="email" id="email" name="email"placeholder="Your email here" required/>
+                <input className="w-100" type="email" id="email" name="email"placeholder="Your email here" required/>
                 <label for="message">Message:</label>
                 <textarea id="message" name="message" placeholder="Write your message here" rows="4" required></textarea>
-                <div className="d-flex justify-content-end">
+                <div className="d-flex justify-content-end mb-3">
                 <button type="submit" className="send btn btn-success" >Send</button>
                 </div>
+            </div>
             </form>
        
             </div>     
